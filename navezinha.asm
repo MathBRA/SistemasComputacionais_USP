7a0cc7195cdf15dff15b14927371a025042076f7
matheus.rf <114034604+MathBRA@users.noreply.github.com>
2023-07-10 20:58:30 -0300

    Add files via upload
    Fazendo um caracter (nave) se movimentar pela tela.

---
+++ navezinha.asm
@@ -0,0 +1,104 @@
+; Hello World - Escreve mensagem armazenada na memoria na tela
+
+
+; ------- TABELA DE CORES -------
+; adicione ao caracter para Selecionar a cor correspondente
+
+; 0 branco							0000 0000
+; 256 marrom						0001 0000
+; 512 verde							0010 0000
+; 768 oliva							0011 0000
+; 1024 azul marinho					0100 0000
+; 1280 roxo							0101 0000
+; 1536 teal							0110 0000
+; 1792 prata						0111 0000
+; 2048 cinza						1000 0000
+; 2304 vermelho						1001 0000
+; 2560 lima							1010 0000
+; 2816 amarelo						1011 0000
+; 3072 azul							1100 0000
+; 3328 rosa							1101 0000
+; 3584 aqua							1110 0000
+; 3840 branco						1111 0000
+
+
+
+jmp main
+
+posAnt : var #10
+posNova : var #10
+
+;---- Inicio do Programa Principal -----
+
+main:
+	loadn r0, #1199	; Posicao na tela onde a mensagem sera' escrita
+	store posAnt, r0	; Carrega r1 com o endereco do vetor que contem a mensagem
+	store posNova, r0	; Seleciona a COR da Mensagem
+	   
+	loadn r0, #' '
+	loadn r2, #'X'
+	call Loop
+	halt
+	
+;---- Fim do Programa Principal -----
+	
+;---- Inicio das Subrotinas -----
+Loop:
+	load r1, posAnt
+	outchar r0, r1
+	
+	dec r1
+	
+	outchar r2, r1
+	store posAnt, r1
+	call Delay
+	jmp Loop
+
+Delay:
+						;Utiliza Push e Pop para nao afetar os Ristradores do programa principal
+	push r0
+	push r1
+	
+	loadn r1, #200  ; a
+   	Delay_volta2:				; contador de tempo quebrado em duas partes (dois loops de decremento)
+		loadn r0, #3000	; b
+   	Delay_volta: 
+		dec r0					; (4*a + 6)b = 1000000  == 1 seg  em um clock de 1MHz
+		jnz Delay_volta	
+		dec r1
+		jnz Delay_volta2
+	
+	pop r1
+	pop r0
+	
+	rts
+								
+Imprimestr:	;  Rotina de Impresao de Mensagens:    r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
+	push r0	; protege o r0 na pilha para preservar seu valor
+	push r1	; protege o r1 na pilha para preservar seu valor
+	push r2	; protege o r1 na pilha para preservar seu valor
+	push r3	; protege o r3 na pilha para ser usado na subrotina
+	push r4	; protege o r4 na pilha para ser usado na subrotina
+	
+	loadn r3, #'\0'	; Criterio de parada
+
+ImprimestrLoop:	
+	loadi r4, r1
+	cmp r4, r3
+	jeq ImprimestrSai
+	add r4, r2, r4
+	outchar r4, r0
+	inc r0
+	inc r1
+	jmp ImprimestrLoop
+	
+ImprimestrSai:	
+	pop r4	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
+	
+	
+	
